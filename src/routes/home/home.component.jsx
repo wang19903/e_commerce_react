1131f33f42f200b87a2692a2f4a0cc2c08e11567
wang19903 <lf21715@yahoo.com.tw>
2022-10-24 16:21:47 +0800

    Update home.component.jsx

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,7 +1,13 @@
 import Directory from "../../components/directory/directory.component";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
-  return <Directory />;
+  return (
+    <div>
+      <Directory />
+      <Outlet />
+    </div>
+  );
 };
 
 export default Home;

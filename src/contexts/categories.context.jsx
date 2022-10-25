c33eba59088c7cd9dd407512a5056f0ab1b44f01
wang19903 <lf21715@yahoo.com.tw>
2022-10-25 11:44:21 +0800

    test2

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,7 +1,6 @@
 import { createContext, useState, useEffect } from "react";
+
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
-//import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
-// import SHOP_DATA from "../shop-data.js"; //set in firebase
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
@@ -9,21 +8,17 @@ export const CategoriesContext = createContext({
 
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
-  const value = { categoriesMap };
-
-  // useEffect(() => {
-  //   addCollectionAndDocuments("categories", SHOP_DATA);
-  // }, []); //set in firebase
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      console.log(categoryMap);
+      const categoryMap = await getCategoriesAndDocuments("categories");
       setCategoriesMap(categoryMap);
     };
-    return getCategoriesMap;
+
+    getCategoriesMap();
   }, []);
 
+  const value = { categoriesMap };
   return (
     <CategoriesContext.Provider value={value}>
       {children}

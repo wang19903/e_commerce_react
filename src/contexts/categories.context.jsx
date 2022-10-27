54bbc225137bf0695e81e6f711a0ce498337c913
wang19903 <lf21715@yahoo.com.tw>
2022-10-27 11:45:31 +0800

    firestore reducer
    move firestore category .reduce() to reducer for optimization

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,32 +1,33 @@
-import { createContext, useState, useEffect } from "react";
-import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
-//import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
-// import SHOP_DATA from "../shop-data.js"; //set in firebase
+//note
+// import { createContext, useState, useEffect } from "react";
+// import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+// //import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
+// // import SHOP_DATA from "../shop-data.js"; //set in firebase
 
-export const CategoriesContext = createContext({
-  categoriesMap: {},
-});
+// export const CategoriesContext = createContext({
+//   categoriesMap: {},
+// });
 
-export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+// export const CategoriesProvider = ({ children }) => {
+//   const [categoriesMap, setCategoriesMap] = useState({});
 
-  // useEffect(() => {
-  //   addCollectionAndDocuments("categories", SHOP_DATA);
-  // }, []); //set in firebase
+//   // useEffect(() => {
+//   //   addCollectionAndDocuments("categories", SHOP_DATA);
+//   // }, []); //set in firebase
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      //   console.log(categoryMap);
-      setCategoriesMap(categoryMap);
-    };
-    getCategoriesMap();
-  }, []);
+//   useEffect(() => {
+//     const getCategoriesMap = async () => {
+//       const categoryMap = await getCategoriesAndDocuments();
+//       //   console.log(categoryMap);
+//       setCategoriesMap(categoryMap);
+//     };
+//     getCategoriesMap();
+//   }, []);
 
-  const value = { categoriesMap };
-  return (
-    <CategoriesContext.Provider value={value}>
-      {children}
-    </CategoriesContext.Provider>
-  );
-};
+//   const value = { categoriesMap };
+//   return (
+//     <CategoriesContext.Provider value={value}>
+//       {children}
+//     </CategoriesContext.Provider>
+//   );
+// };
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,14 +1,14 @@
-import { useParams } from "react-router-dom";
 import { useState, useEffect, Fragment } from "react";
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/product-card.component";
 import { CategoryContainer, CategoryTitle } from "./category.styles";
 import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 const Category = () => {
   const { category } = useParams();
-  const [products, setProducts] = useState(categoriesMap[category]);
   const categoriesMap = useSelector(selectCategoriesMap);
+  const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
     setProducts(categoriesMap[category]);

3ff3a5df5c81f92268afa7ceb8bd135a44ed9aa2
wangg <lf21715@yahoo.com.tw>
2022-10-29 19:34:18 +0800

    saga-category

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,26 +1,24 @@
 import { CATEGORY_ACTION_TYPES } from "./category.type";
 import { createAction } from "../../utils/reducer/reducer.utils";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORY_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
 
 export const fetchCategoriesStart = () =>
   createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START);
 
 export const fetchCategoriesSuccess = (categoriesArray) =>
   createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+
 export const fetchCategoriesfailed = (error) =>
   createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
 
-export const fetchCategoriesAsync = () => {
-  return async (dispatch) => {
-    dispatch(fetchCategoriesStart());
-    try {
-      const categoriesArray = await getCategoriesAndDocuments("categories");
-      dispatch(fetchCategoriesSuccess(categoriesArray));
-    } catch (error) {
-      dispatch(fetchCategoriesfailed(error));
-    }
-  };
-};
+// redux-thunk
+//export const fetchCategoriesAsync = () => {
+//   return async (dispatch) => {
+//     dispatch(fetchCategoriesStart());
+//     try {
+//       const categoriesArray = await getCategoriesAndDocuments("categories");
+//       dispatch(fetchCategoriesSuccess(categoriesArray));
+//     } catch (error) {
+//       dispatch(fetchCategoriesfailed(error));
+//     }
+//   };
+// };
---
+++ src/store/categories/category.saga.js
@@ -0,0 +1,27 @@
+import { takeLatest, all, call, put } from "redux-saga/effects";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import {
+  fetchCategoriesSuccess,
+  fetchCategoriesfailed
+} from "./category.action";
+import { CATEGORY_ACTION_TYPES } from "./category.type";
+
+export function* fetchCategoriesAsync() {
+  try {
+    const categoriesArray = yield call(getCategoriesAndDocuments, "categories");
+    yield put(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    yield put(fetchCategoriesfailed(error));
+  }
+}
+
+export function* onFetchCategories() {
+  yield takeLatest(
+    CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START,
+    fetchCategoriesAsync
+  );
+}
+
+export function* categoriesSaga() {
+  yield all([call(onFetchCategories)]);
+}
